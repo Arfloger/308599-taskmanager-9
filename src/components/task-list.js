cdbab8697e40d7b3b45d17181906310dde1813cf
ksenia <arfloger@gmail.com>
2019-08-29 12:23:15 +0300

    test

---
+++ src/components/task-list.js
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+import {AbstractComponent} from "../components/abstract-component.js";
+
+<<<<<<< HEAD
+export default class TaskList extends AbstractComponent {
+  constructor() {
+    super();
+  }
+
+  getTemplate() {
+    return `<div class="board__tasks"></div>`.trim();
+=======
+export class TaskList extends AbstractComponent {
+  getTemplate() {
+    return `<div class="board__tasks"></div>`;
+>>>>>>> 577aadb6f275eed5991445e416b3a5bdcb35257b
+  }
+}
+=======
+>>>>>>> d6070a58c96e535d02640bd6e070b356dcc269f6
