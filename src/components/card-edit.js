c7358460cd5d2f955fbef4c3a6ae6565e53da0d9
ksenia <arfloger@gmail.com>
2019-09-05 23:20:03 +0300

    add task 18

--- src/components/card-edit.js
+++ src/components/card-edit.js
@@ -1,14 +1,19 @@
-import {getRandomValue, compareRandom} from "../utils.js";
 import {AbstractComponent} from "../components/abstract-component.js";
 
 export default class TaskEdit extends AbstractComponent {
-  constructor({description, dueDate, tags, color, repeatingDays}) {
+  constructor({description, dueDate, tags, color, repeatingDays, isFavorite, isArchive, isRepeating, isDate}) {
     super();
     this._description = description;
-    this._dueDate = new Date(dueDate);
+    this._dueDate = new Date(dueDate).toDateString();
     this._tags = tags;
     this._color = color;
+    this._isArchive = isArchive;
+    this._isFavorite = isFavorite;
     this._repeatingDays = repeatingDays;
+    this._isRepeating = isRepeating;
+    this._isDate = isDate;
+    this._subscribeOnEvents();
+    this._setCheckedElements();
   }
 
   getTemplate() {
@@ -20,10 +25,10 @@ export default class TaskEdit extends AbstractComponent {
     <form class="card__form" method="get">
     <div class="card__inner">
         <div class="card__control">
-        <button type="button" class="card__btn card__btn--archive">
+        <button type="button" class="card__btn card__btn--archive ${this._isArchive ? ` card__btn--disabled` : ``}">
             archive
         </button>
-        <button type="button" class="card__btn card__btn--favorites card__btn--disabled">
+        <button type="button" class="card__btn card__btn--favorites ${this._isFavorite ? ` card__btn--disabled` : ``}">
             favorites
         </button>
         </div>
@@ -36,9 +41,10 @@ export default class TaskEdit extends AbstractComponent {
 
         <div class="card__textarea-wrap">
         <label>
-            <textarea class="card__text" placeholder="${
-  this._description
-}" name="text">${this._description}</textarea>
+            <textarea class="card__text" 
+                placeholder="${this._description}" 
+                name="text">${this._description}
+            </textarea>
         </label>
         </div>
 
@@ -46,34 +52,38 @@ export default class TaskEdit extends AbstractComponent {
         <div class="card__details">
             <div class="card__dates">
             <button class="card__date-deadline-toggle" type="button">
-                date: <span class="card__date-status">no</span>
+                date: <span class="card__date-status">${this._isDate ? `yes` : `no`}</span>
             </button>
 
-            <fieldset class="card__date-deadline" disabled="">
+            <fieldset class="card__date-deadline" ${this._isDate ? `` : `disabled`}>
                 <label class="card__input-deadline-wrap">
-                <input class="card__date" type="text" placeholder="23 September" name="date" value="${this._dueDate.toDateString()} ${this._dueDate.getHours()}:${this._dueDate.getMinutes()}">
+                <input class="card__date" 
+                type="text" 
+                placeholder="" 
+                name="date" 
+                value="${this._isDate ? this._dueDate : ``}">
                 </label>
             </fieldset>
 
             <button class="card__repeat-toggle" type="button">
-                repeat:<span class="card__repeat-status">no</span>
+                repeat:<span class="card__repeat-status">${this._isRepeating ? `yes` : `no`}</span>
             </button>
 
-            <fieldset class="card__repeat-days" disabled="">
+            <fieldset class="card__repeat-days" ${this._isRepeating ? `` : `disabled`}>
                 <div class="card__repeat-days-inner">
                 <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-mo-1" name="repeat" value="mo">
                 <label class="card__repeat-day" for="repeat-mo-1">mo</label>
-                <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-tu-1" name="repeat" value="tu" checked="">
+                <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-tu-1" name="repeat" value="tu">
                 <label class="card__repeat-day" for="repeat-tu-1">tu</label>
                 <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-we-1" name="repeat" value="we">
                 <label class="card__repeat-day" for="repeat-we-1">we</label>
                 <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-th-1" name="repeat" value="th">
                 <label class="card__repeat-day" for="repeat-th-1">th</label>
-                <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-fr-1" name="repeat" value="fr" checked="">
+                <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-fr-1" name="repeat" value="fr">
                 <label class="card__repeat-day" for="repeat-fr-1">fr</label>
                 <input class="visually-hidden card__repeat-day-input" type="checkbox" name="repeat" value="sa" id="repeat-sa-1">
                 <label class="card__repeat-day" for="repeat-sa-1">sa</label>
-                <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-su-1" name="repeat" value="su" checked="">
+                <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-su-1" name="repeat" value="su">
                 <label class="card__repeat-day" for="repeat-su-1">su</label>
                 </div>
             </fieldset>
@@ -82,16 +92,21 @@ export default class TaskEdit extends AbstractComponent {
             <div class="card__hashtag">
             <div class="card__hashtag-list">
                     ${Array.from(this._tags)
-                      .sort(compareRandom)
-                      .splice(0, getRandomValue(4))
                       .map(
                           (tag) => `<span class="card__hashtag-inner">
-                      <span class="card__hashtag-name">
-                      #${tag}
-                      </span>
-                      </span>`
+                          <input type="hidden" name="hashtag" value="${tag}" class="card__hashtag-hidden-input">
+                          <p class="card__hashtag-name">
+                            #${tag}
+                          </p>
+                          <button type="button" class="card__hashtag-delete">
+                            delete
+                          </button>
+                        </span>`
                       )
                       .join(``)}
+
+
+
                 </div>
 
             <label>
@@ -103,17 +118,23 @@ export default class TaskEdit extends AbstractComponent {
         <div class="card__colors-inner">
             <h3 class="card__colors-title">Color</h3>
             <div class="card__colors-wrap">
+
             <input type="radio" id="color-black-1" class="card__color-input card__color-input--black visually-hidden" name="color" value="black" checked="">
             <label for="color-black-1" class="card__color card__color--black">black</label>
+
             <input type="radio" id="color-yellow-1" class="card__color-input card__color-input--yellow visually-hidden" name="color" value="yellow">
             <label for="color-yellow-1" class="card__color card__color--yellow">yellow</label>
+
             <input type="radio" id="color-blue-1" class="card__color-input card__color-input--blue visually-hidden" name="color" value="blue">
             <label for="color-blue-1" class="card__color card__color--blue">blue</label>
+
             <input type="radio" id="color-green-1" class="card__color-input card__color-input--green visually-hidden" name="color" value="green">
             <label for="color-green-1" class="card__color card__color--green">green</label>
+
             <input type="radio" id="color-pink-1" class="card__color-input card__color-input--pink visually-hidden" name="color" value="pink">
             <label for="color-pink-1" class="card__color card__color--pink">pink</label>
             </div>
+
         </div>
         </div>
 
@@ -124,5 +145,48 @@ export default class TaskEdit extends AbstractComponent {
     </div>
     </form>
   </article>`.trim();
+  }
+
+  _setCheckedElements() {
+
+    const repeatCheckboxElements = this.getElement().querySelectorAll(`.card__repeat-day-input`);
+    const repeatingDays = Object.values(this._repeatingDays);
+    repeatingDays.map((isRepeat, index) => {
+      isRepeat ? repeatCheckboxElements[index].checked = true : ``;
+    });
+  }
+
+  _subscribeOnEvents() {
+
+    this.getElement().querySelector(`.card__color-input--${this._color}`).checked = true;
+
+    this.getElement()
+        .querySelector(`.card__hashtag-input`).addEventListener(`keydown`, (evt) => {
+          if (evt.key === `Enter`) {
+            evt.preventDefault();
+            this.getElement().querySelector(`.card__hashtag-list`).insertAdjacentHTML(`beforeend`, `<span class="card__hashtag-inner">
+        <input
+          type="hidden"
+          name="hashtag"
+          value="${evt.target.value}"
+          class="card__hashtag-hidden-input"
+        />
+        <p class="card__hashtag-name">
+          #${evt.target.value}
+        </p>
+        <button type="button" class="card__hashtag-delete">
+          delete
+        </button>
+      </span>`);
+            evt.target.value = ``;
+          }
+        });
+
+    (document).addEventListener(`click`, (evt) => {
+      if (evt.target.classList.contains(`card__hashtag-delete`)) {
+        evt.preventDefault();
+        evt.target.closest(`.card__hashtag-inner`).remove();
+      }
+    });
   }
 }
